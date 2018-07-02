d1cd72eec377f421009827c4dcdeabe14705af63
EmmaRamirez <ramirez.emma.g@gmail.com>
2018-07-02 00:18:24 -0600

    add git config

---
+++ git/aliases.zsh
@@ -0,0 +1,7 @@
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gl='git pull --prune'
+
+alias ci='git commit'
+alias co='git checkout'
+alias br='git branch'
+alias p='git push'
